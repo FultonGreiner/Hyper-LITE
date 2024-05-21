8832addf57112947839b689f0fa483da02d9e4d0
Fulton Greiner <fultongreiner@gmail.com>
2024-05-21 12:05:57 -0500

    Updated build config and debug/run scripts

---
+++ debug_hypervisor.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "."
+
+qemu-system-aarch64 \
+    -machine virt,virtualization=on \
+    -cpu cortex-a53 \
+    -nographic \
+    -smp 1 \
+    -m 2048 \
+    -serial mon:stdio \
+    -S -s \
+    -kernel build/Hyper-LITE
--- run_hypervisor.sh
+++ run_hypervisor.sh
@@ -6,8 +6,18 @@ qemu-system-aarch64 \
     -cpu cortex-a53 \
     -nographic \
     -smp 1 \
-    -m 1024 \
+    -m 2048 \
     -kernel build/Hyper-LITE \
     -serial mon:stdio \
     -monitor none \
     -no-reboot
+
+# qemu-system-aarch64 \
+#     -machine raspi3b \
+#     -cpu cortex-a53 \
+#     -m 1024 \
+#     -serial mon:stdio \
+#     -kernel build/Hyper-LITE \
+#     -append "console=ttyAMA0,115200 root=/dev/mmcblk0p2 rw rootwait" \
+#     -smp 4 \
+#     -nographic
